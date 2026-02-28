cd5166fd300c68ad6debed8f5b8fe39a3c88b8a2
KNSN92 <kekenshin92@gmail.com>
2026-02-28 23:25:37 +0900

    update: バージョンを0.2.0にアプグレ

--- src/main/java/com/knsn92/ugorge/Ugorge.java
+++ src/main/java/com/knsn92/ugorge/Ugorge.java
@@ -22,7 +22,7 @@ public class Ugorge {
 
     public static final String MOD_ID = "Ugorge";
     public static final String MOD_NAME = "Ugorge";
-    public static final String MOD_VERSION = "0.1.0";
+    public static final String MOD_VERSION = "0.2.0";
 
     public static boolean isDevEnv = (Boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment");
 
